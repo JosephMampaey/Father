aabb75ceba147e033e5a3c53e5cc2a881ebe5d23
Jef Jacobs <jef.jacobs@student.odisee.be>
2020-08-27 21:52:40 +0200

    Kill meh, yeh?

--- src/main/java/Bot/Event.java
+++ src/main/java/Bot/Event.java
@@ -1,8 +1,15 @@
 package Bot;
 
+import Logica.Person;
+import Logica.Server;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
@@ -12,6 +19,9 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 //TODO Make command lose coupled (Easy to add a command)
 
 public class Event extends ListenerAdapter {
+    
+    Dictionary<String, Server> servers =  new Hashtable<String, Server>();
+    
     public Event() {
     }
 
@@ -27,9 +37,16 @@ public class Event extends ListenerAdapter {
         if(receivedMessageRaw.contains(""+prefix)){
             int possitionPrefix = receivedMessageRaw.indexOf(prefix);
             if(getNumberFromCommand(receivedMessageRaw) > 0){
-                checkCommand(receivedMessageRaw.substring(possitionPrefix + 1), getNumberFromCommand(receivedMessageRaw));
+                checkCommand(
+                        receivedMessageRaw.substring(possitionPrefix + 1), 
+                        author, 
+                        getNumberFromCommand(receivedMessageRaw),
+                        getReceiver(event.getMessage(), event.getGuild().getId()));
             }else{
-                checkCommand(receivedMessageRaw.substring(possitionPrefix + 1));
+                checkCommand(
+                        receivedMessageRaw.substring(possitionPrefix + 1), 
+                        author, 
+                        getReceiver(event.getMessage(), event.getGuild().getId()));
             }
         }
     }
@@ -44,23 +61,65 @@ public class Event extends ListenerAdapter {
     }
 
 
-    //TODO make this lose coupled
     //TODO change the where you check if the person is allowed to give points
-    private void checkCommand(String command){
-        switch (command){
-            case "AddGBP":
+    private void checkCommand(String command,Member author, ArrayList<Person> receivers){
+        switch (command.toLowerCase()){
+            case "addpoints":
+                if(allowedToGivePoints(author)) {
+                    for (int i = 0; i < receivers.size(); i++) {
+                        receivers.get(i).AddPoint(1);
+                    }
+                }
                 break;
-            case "RMGBP":
+            case "rmpoints":
+                if(allowedToGivePoints(author)) {
+                    for (int i = 0; i < receivers.size(); i++) {
+                        receivers.get(i).RemovePoint(1);
+                    }
+                }
+                break;
+            case "points":
+                if(allowedToGivePoints(author)) {
+                    for (int i = 0; i < receivers.size(); i++) {
+                        receivers.get(i).GetPoints();
+                    }
+                }
+                break;
+            case "settings":
+                if(allowedToGivePoints(author)) {}
+                break;
+            case "set":
+                if(allowedToGivePoints(author)) {}
+                break;
+            case "clear":
+                if(allowedToGivePoints(author)) {}
+                break;
+            default:
                 break;
         }
     }
 
-    //TODO make this lose coupled
-    private void checkCommand(String command, int ammount){
-        switch (command){
-            case "AddGBP":
+
+    private void checkCommand(String command,Member author, int ammount, ArrayList<Person> receivers){
+       switch (command.toLowerCase()){
+            case "addpoints":
+                if(allowedToGivePoints(author)) {
+                    for (int i = 0; i < receivers.size(); i++) {
+                        receivers.get(i).AddPoint(1);
+                    }
+                }
                 break;
-            case "RMGBP":
+            case "rmpoints":
+                if(allowedToGivePoints(author)) {
+                    for (int i = 0; i < receivers.size(); i++) {
+                        receivers.get(i).RemovePoint(1);
+                    }
+                }
+                break;
+            case "set":
+                if(allowedToGivePoints(author)) {}
+                break;
+            default:
                 break;
         }
     }
@@ -77,5 +136,19 @@ public class Event extends ListenerAdapter {
         }
         return -1;
     }
+    
+    //TODO Add person to the server if it doesnt exist
+    private ArrayList<Person> getReceiver(Message message, String serverId){
+        ArrayList<Person> receivers = new ArrayList<Person>();
+        if(message.getMentionedUsers().size() > 0){
+            for (int i = 0; i < message.getMentionedUsers().size(); i++) {
+                receivers.add(servers.get(serverId).getMembers().get(message.getMentionedUsers().get(i).getId()));
+            }
+        }
+        return receivers;
+    }
+    
+    
+
 
 }
